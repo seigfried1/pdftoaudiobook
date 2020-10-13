9624500bfc20161a82d676751d102963cf771cbe
Rahul Mishra <rahul.mishra2690@gmail.com>
2020-10-13 12:18:26 +0530

    Added docstrings

--- pdftoAudiobook/pdftoAudiobook.py
+++ pdftoAudiobook/pdftoAudiobook.py
@@ -4,6 +4,10 @@ import os
 import time
 
 def pdf_to_audiobook(pdfname):
+	"""
+	Takes one string argument, that is, pdf file path name and 
+	returns an audiobook as a single .mp3 file
+	"""
 	language = 'en'
 	file = open(pdfname, 'rb')
 	fileReader = PyPDF2.PdfFileReader(file)
@@ -19,6 +23,10 @@ def pdf_to_audiobook(pdfname):
 	return True
 
 def pdf_to_audiobook_per_page(pdfname):
+	"""
+	Take one string argument pdf file, that is, pdf file path name and
+	returns one .mp3 file per page. 
+	"""
 	language = 'en'
 	file = open(pdfname, 'rb')
 	fileReader = PyPDF2.PdfFileReader(file)
@@ -32,10 +40,3 @@ def pdf_to_audiobook_per_page(pdfname):
 		myobj = gTTS(text=finalText, lang=language, slow=False)
 		myobj.save("{}.mp3".format(i))
 	return True
-
-# print(pdf_to_audiobook('marieVF.pdf'))
-
-
-
-
-# from pdftoAudiobook.pdftoAudiobook import *
